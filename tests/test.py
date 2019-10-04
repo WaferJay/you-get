d8e99c2a3b2436e81f46ebb6e68de0b8d340e718
WaferJay <474346873@qq.com>
2019-10-04 20:34:13 +0800

    [missevan test] test album download

--- tests/test.py
+++ tests/test.py
@@ -12,6 +12,8 @@ class YouGetTests(unittest.TestCase):
     def test_missevan(self):
         missevan.download('https://m.missevan.com/sound/1285995', info_only=True)
         missevan.download_playlist(
+            'https://www.missevan.com/albuminfo/57815', info_only=True)
+        missevan.download_playlist(
             'https://www.missevan.com/mdrama/drama/18129', info_only=True)
 
 if __name__ == '__main__':
