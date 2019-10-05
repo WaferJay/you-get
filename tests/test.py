885e4904498a22d9749b3efce1bddabc13631b34
WaferJay <474346873@qq.com>
2019-10-05 16:48:41 +0800

    [missevan] replace album link and add a drama link in test case

--- tests/test.py
+++ tests/test.py
@@ -11,6 +11,10 @@ class YouGetTests(unittest.TestCase):
 
     def test_missevan(self):
         missevan.download('https://m.missevan.com/sound/1221539', info_only=True)
+        missevan.download_playlist(
+            'https://www.missevan.com/albuminfo/203090', info_only=True)
+        missevan.download_playlist(
+            'https://www.missevan.com/mdrama/drama/24130', info_only=True)
 
 if __name__ == '__main__':
     unittest.main()
